c2d32fe0339ef964d31d41e6cf956f2d2ca8a4bb
Kaleo Tanasale <43254219+gizmosity@users.noreply.github.com>
2018-10-18 13:30:52 -0700

    Update height lab.c

--- height lab.c
+++ height lab.c
@@ -20,23 +20,25 @@ int main () {
 	scanf("%d", &i);
 
 
+	h = f * 12 + i;
+	
 	if (g == 'm') {
 
-		if (f == 5 && i == 9) {printf("Your height is just about average for a male.");}
+		if (h == 69) {printf("Your height is just about average for a male.");}
 		
-		if (f >= 5 && i > 9 || (i == 0 && f > 5)) {printf("Your height is above average for a male");}
+		if (h > 69) {printf("Your height is above average for a male");}
 		
-		if (f <= 5 && i < 9 || (f <= 4 && i <= 11)) {printf("Your height is below average for a male");}
+		if (h < 69) {printf("Your height is below average for a male");}
 
 	}
 
 	else if (g == 'f') {
 
-		if (f == 5 && i == 4) {printf("Your height is just about average for a female.");}
+		if (h == 64) {printf("Your height is just about average for a female.");}
 		
-		if (f >= 5 && i > 4 || (i == 0 && f > 5)) {printf("Your height is above average for a female");}
+		if (h > 64) {printf("Your height is above average for a female");}
 
-		if (f <= 5 && i < 4 || (f <= 4 && i <= 11)) {printf("Your height is below average for a female");}
+		if (h < 64) {printf("Your height is below average for a female");}
 	}
 
 	return 0;
